1e2044bf57f6468b213108bf8e4b3bd1200beb0c
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-06-19 15:54:58 +0100

    Implete characters application - Add actions to action files - Add reducers - Map state to props

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,48 @@
 // we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+function fetching(isFetching) {
+    return {
+        type: FETCHING,
+        payload: isFetching,
+    };
+}
+
+function success(data) {
+    return {
+        type: SUCCESS,
+        payload: data,
+    };
+}
+
+function failure(error) {
+    return {
+        type: FAILURE,
+        payload: error,
+    };
+}
+
+export const fetchPeopleData = () => dispatch => {
+    dispatch(fetching(true))
+    axios.get('https://swapi.co/api/people/')
+        .then(res => {
+            dispatch(success(res.data.results))
+        })
+        .catch(error => {
+            console.log('error.message');
+            dispatch(failure(error.message))
+        }).
+        finally(() => {
+            dispatch(fetching(false))
+        })
+}
